5b8ed7fd16f47ce68cbf0356fa1135580600c0e8
Evelin Arust <evelin.arust@khk.ee>
2018-09-16 22:42:30 +0300

    Creating a database

--- web/index.php
+++ web/index.php
@@ -12,6 +12,13 @@ $app->register(new Silex\Provider\TwigServiceProvider(), [
 'twig.path' => __DIR__.'/../views',
 ]);
 
+$app->register(new Silex\Provider\DoctrineServiceProvider(), [
+    'db.options' => [
+        'driver' => 'pdo_sqlite',
+        'path' => __DIR__.'/../database/app.db',
+    ],
+]);
+
 $app->get('/bookings/create', function () use ($app) {
     return $app['twig']->render('base.html.twig');
     });
